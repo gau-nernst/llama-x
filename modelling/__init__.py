50113ffb724ffbd5faa4a434ea90e3a2bdafd1a4
Thien Tran <gau.nernst@yahoo.com.sg>
2024-08-19 09:03:53 +0800

    add LlamaAudio

--- modelling/__init__.py
+++ modelling/__init__.py
@@ -1,2 +1,3 @@
+from .audio import AudioConfig, LlamaAudio3_1
 from .int8_lora import Int8LoRALinear
-from .llama3_1 import Llama3_1, Llama3Tokenizer, llama3_1_4b, llama3_1_8b
+from .llama3_1 import Llama3_1, Llama3_1Config, Llama3Tokenizer, llama3_1_4b, llama3_1_8b
---
+++ modelling/audio.py
@@ -0,0 +1,61 @@
+from typing import NamedTuple
+
+import torch
+from torch import Tensor, nn
+from torch.utils.checkpoint import checkpoint
+from torchaudio.transforms import MelSpectrogram
+
+from .llama3_1 import Llama3_1, Llama3_1Config
+
+
+class AudioConfig(NamedTuple):
+    sample_rate: int = 16_000
+    n_fft: int = 512
+    win_length: int = 400
+    hop_length: int = 160
+    n_mels: int = 128
+
+
+class LlamaAudio3_1(Llama3_1):
+    def __init__(self, config: Llama3_1Config, audio_config: AudioConfig = AudioConfig()):
+        super().__init__(config)
+        self.audio_config = audio_config
+
+        # inspired by Whisper encoder
+        self.conv = nn.Sequential(
+            nn.Conv1d(audio_config.n_mels, config.embed_dim, 3, 1, 1),
+            nn.GELU(),
+            nn.Conv1d(config.embed_dim, config.embed_dim, 3, 2, 1),
+            nn.GELU(),
+        )
+
+    def build_cache(self):
+        super().build_cache()
+        self.melspec = MelSpectrogram(**self.audio_config._asdict())
+
+    def forward(
+        self,
+        audio: Tensor,
+        tokens: Tensor,
+        *,
+        mask: Tensor | None = None,
+        input_pos: Tensor | None = None,
+    ) -> Tensor:
+        # we need to slice the last time step to make it a nice multiple
+        audio = self.melspec(audio)[..., :-1].clip(1e-12).log()  # (B, n_mels, L)
+        audio = audio - audio.mean(2, keepdim=True)  # cmn
+        audio = audio.to(dtype=self.conv[0].weight.dtype)
+        audio = self.conv(audio).transpose(1, 2)
+
+        tok_embs = self.tok_embeddings(tokens)
+        x = torch.cat([audio, tok_embs], dim=1)
+
+        rope = self.rope[: x.shape[1]]
+        for layer in self.layers:
+            if self.config.activation_checkpointing:
+                x = checkpoint(layer, x, rope, mask=mask, input_pos=input_pos, use_reentrant=False)
+            else:
+                x = layer(x, rope, mask=mask, input_pos=input_pos)
+        x = self.norm(x)
+        x = self.output(x)
+        return x
